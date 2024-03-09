f73a6bc3e11d4c9be1d2cc30e00737cccef2cc47
amir fattahi <amirfattahi22@gmail.com>
2024-03-09 17:02:14 +0330

    Add files via upload

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+      <div className='flex-1 pt-36 padding-x'></div>
+      hello
+    </div>
+  )
+}
+
+export default Hero

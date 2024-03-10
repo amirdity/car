98f741f0073bfc8bff630cfecdec753725cc405a
amir fattahi <amirfattahi22@gmail.com>
2024-03-10 09:38:43 +0330

    Add files via upload

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,8 +10,8 @@ const Hero = () => {
         <h1 className='hero__title'>
           Find, book, or rent a car - quickly and easily!
         </h1>
-        <p>
-          /streamline your car rental experience with our effortless booking process.
+        <p className='hero__subtitle'>
+          streamline your car rental experience with our effortless booking process.
         </p>
         <CustomButton title="Explore Cars" containerStyles="bg-primary-blue text-white rounded-full mt-10" handleClick={handleScroll}/>
       </div>

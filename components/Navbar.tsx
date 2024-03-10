3cceb895190c88d46b660acae4c2725129649b41
amir fattahi <amirfattahi22@gmail.com>
2024-03-10 08:30:37 +0330

    Add files via upload

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import Image from 'next/image'
+import  CustomButton from './CustomButton'
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <header className='w-full absolute z-10'>
+      <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4'></nav>
+    </header>
+
   )
 }
 
